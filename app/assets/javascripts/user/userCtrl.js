a4f6a3a92aa4ceaa22a38db1477289729e59f0f0
Thomas <thomasiscooly@gmail.com>
2015-08-05 19:38:34 -0400

    fix api key update front end

--- app/assets/javascripts/user/userCtrl.js
+++ app/assets/javascripts/user/userCtrl.js
@@ -1,10 +1,23 @@
 app.controller('UserCtrl', [
   '$scope',
    'Auth',
-   function($scope, Auth){
+   '$http',
+   function($scope, Auth, $http){
+
+    Auth.currentUser().then(function (user){
+      $scope.user = user;
+    });
 
     $scope.updateWaniKaniInfo = function(){
       alert('update?');
+    };
+
+    $scope.updateApiKey = function(current_user){
+      return $http.put({
+        method: 'PUT',
+        url: '/users/' + current_user.id + '.json',
+        data: {"current_user.api_key": $scope.api_key}
+      });
     };
 
    }
