33093679b5d6573ab8ac8c0c4ab124e537ec1011
Thomas <thomasiscooly@gmail.com>
2015-08-07 18:36:55 -0400

    working on timeout delay for homepage

--- app/assets/javascripts/home/MainCtrl.js
+++ app/assets/javascripts/home/MainCtrl.js
@@ -1,20 +1,31 @@
 app.controller('MainCtrl', [
   '$scope', 
   'Auth',
-  function($scope, Auth){
+  '$timeout',
+  function($scope, Auth, $timeout){
 
     $scope.signedIn = Auth.isAuthenticated;
+    $scope.user = {};
+    $scope.username = '';
 
     Auth.currentUser().then(function (user){
       $scope.user = user;
     });
 
     var get_username = function() {
-      var info = angular.fromJson($scope.user.wanikani_user_info);
+      var info;
+      try{
+        info = JSON.parse($scope.user.wanikani_user_info);
+      }catch(e){
+        alert(e);
+      }
+      alert(info['username']);
       return info['username'];
     };
 
-    //$scope.username = get_username();
+    $scope.username = $timeout(function(){ 
+      get_username();
+      }, 50000);
 
   }
 ]);
