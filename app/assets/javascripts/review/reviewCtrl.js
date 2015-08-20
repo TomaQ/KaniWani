8db9b3ca4001ee16eb0a51651e63d44475e85e29
Thomas <thomasiscooly@gmail.com>
2015-08-20 18:51:15 -0400

    add preview of correct review methods

--- app/assets/javascripts/review/reviewCtrl.js
+++ app/assets/javascripts/review/reviewCtrl.js
@@ -5,29 +5,28 @@ app.controller('ReviewCtrl', [
    function($scope, Auth, Review){
 
     $scope.user;
-    $scope.review_words; //= get_review_words();
+    $scope.review_words;
+    $scope.chosen_word;
 
     Auth.currentUser().then(function (user){
       $scope.user = user;
 
       Review.get_review({id: $scope.user.id},function(value){
         $scope.review_words = value;
+        $scope.chosen_word = $scope.review_words[Math.floor(Math.random()*$scope.review_words.length)];
       });
 
     });
 
     $scope.submitReview = function(){
-      alert($scope.user.username);
+      if($scope.chosen_word.character == $scope.user_input){
+        alert("YAY!");
+      }
+      else {
+        alert("no... " + $scope.user_input + " is not " + $scope.chosen_word.character);
+      }
+      $scope.user_input = "";
     };
-
-    /*get_review_words = function(){
-      Review.get_review({id: $scope.user.id}, function(){
-      }, function(error) {
-        console.log(error)
-      });
-    };*/
-
-    
     
    }
 ]);
