d5203fde51b6a0f24b1aeac8eb6574ef5bb074ec
Thomas <thomasiscooly@gmail.com>
2015-08-21 19:19:21 -0400

    add reviewing set of words on review page feature

--- app/assets/javascripts/review/reviewCtrl.js
+++ app/assets/javascripts/review/reviewCtrl.js
@@ -1,12 +1,15 @@
 app.controller('ReviewCtrl', [
   '$scope',
-   'Auth',
-   'Review',
-   function($scope, Auth, Review){
+  'Auth',
+  'Review',
+  function($scope, Auth, Review){
 
     $scope.user;
     $scope.review_words;
     $scope.chosen_word;
+
+    var correct_words = 0;
+    var remaining_words; //words that are left in the review_set
 
     Auth.currentUser().then(function (user){
       $scope.user = user;
@@ -14,19 +17,42 @@ app.controller('ReviewCtrl', [
       Review.get_review({id: $scope.user.id},function(value){
         $scope.review_words = value;
         $scope.chosen_word = $scope.review_words[Math.floor(Math.random()*$scope.review_words.length)];
+        remaining_words = $scope.review_words.slice(); //copies the array
       });
 
     });
 
     $scope.submitReview = function(){
-      if($scope.chosen_word.character == $scope.user_input){
+      getReviewSet();
+    };
+
+    function getReviewSet(){
+
+      if(checkWord($scope.chosen_word)){
+        var index = remaining_words.indexOf($scope.chosen_word);
+        remaining_words.splice(index, 1);
+        correct_words += 1;
+      }
+
+      $scope.chosen_word = remaining_words[Math.floor(Math.random()*remaining_words.length)];
+      
+      if(correct_words == 5){ //5 for now
+        alert("You're done!");
+      }
+    }
+
+    function checkWord(word){
+      if(word.character == $scope.user_input){
         alert("YAY!");
+        $scope.user_input = "";
+        return true;
       }
       else {
         alert("no... " + $scope.user_input + " is not " + $scope.chosen_word.character);
+        $scope.user_input = "";
+        return false;
       }
-      $scope.user_input = "";
-    };
+    }
     
-   }
+  }
 ]);
