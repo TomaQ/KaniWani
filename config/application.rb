a11ffd92fbbee9d4687ca9d5616078349a62852f
Thomas <thomasiscooly@gmail.com>
2015-08-21 15:33:02 -0400

    removing line in app.rb

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module KaniWani
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","*")
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
