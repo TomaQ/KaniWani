7a2317e6f24abaf94cc39b7c307e81d1857ac0f9
Thomas <thomasiscooly@gmail.com>
2015-08-21 15:31:31 -0400

    app.rb configuring

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module KaniWani
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","*")
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
 
